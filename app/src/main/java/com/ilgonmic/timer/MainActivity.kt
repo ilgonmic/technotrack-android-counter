0f9cd34314e816e018a74b7731dc0aa93a575bcc
ilgonmic <ilgonmic@gmail.com>
2018-10-28 22:30:02 +0300

    Add timer on first activity

--- app/src/main/java/com/ilgonmic/timer/MainActivity.kt
+++ app/src/main/java/com/ilgonmic/timer/MainActivity.kt
@@ -2,15 +2,35 @@ package com.ilgonmic.timer
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var timer: CountDownTimer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
-        val intent = Intent(this, CountActivity::class.java)
-        startActivity(intent)
+    override fun onResume() {
+        super.onResume()
+
+        timer = object : CountDownTimer(2_000, 100) {
+            override fun onFinish() {
+                val intent = Intent(this@MainActivity, CountActivity::class.java)
+                startActivity(intent)
+            }
+
+            override fun onTick(millisUntilFinished: Long) {
+            }
+
+        }.start()
+    }
+
+    override fun onDestroy() {
+        timer.cancel()
+        super.onDestroy()
     }
 }
